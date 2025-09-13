c06832d10677fc12bad290e4da90f8e9ecebddef
Alok4D <alokroy602701@gmail.com>
2025-09-14 00:06:13 +0600

    home page complete

--- src/Component/Pages/Home/Home.jsx
+++ src/Component/Pages/Home/Home.jsx
@@ -1,11 +1,30 @@
 
+import BestSeller from "../../BestSeller/BestSeller";
+import LatestNews from "../../LatestNews/LatestNews";
 import Banner from "./HomePage/Banner/Banner";
+import CardSection from "./HomePage/CardSection/CardSection";
+import Contact from "./HomePage/Contact/Contact";
+import FeatureProduct from "./HomePage/FeatureProduct/FeatureProduct";
+import Review from "./HomePage/Review/Review";
+import ServiceHighlights from "./HomePage/ServiceHighlights/ServiceHighlights";
+import Testimonial from "./HomePage/Testimonial/Testimonial";
+import TopCategories from "./HomePage/TopCategories/TopCategories";
+import Trusted from "./HomePage/Trusted/Trusted";
 
 const Home = () => {
   return (
     <div>
       <Banner/>
-      
+      <ServiceHighlights/>
+      <FeatureProduct/>
+      <TopCategories/>
+      <Trusted/>
+      <Review/>
+      <CardSection/>
+      <BestSeller/>
+      <LatestNews/>
+      <Testimonial/>
+      <Contact/>
     </div>
   );
 };
